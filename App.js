2ce067c5a71ef5defb01a672809b6fdd4b0fdf36
Erlan909 <erlan.esenbekov054@gmail.com>
2021-06-22 15:08:10 +0600

    GiphyNative

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, StyleSheet, View, Image, Text, TextInput, ScrollView } from "react-native";
+import { API, KEY } from "./Config/Config.js";
 
 export default function App() {
+
+  const [val, setVal] = useState('')
+  const [data, setData] = useState([])
+
+  const searchGyphy = async () => {
+    const req = await fetch(API + val + KEY)
+    const res = await req.json()
+    setData(res.data)
+  }
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <TextInput
+        style={styles.input}
+        value={val}
+        onChangeText={(text) => setVal(text)}
+      />
+      <Button
+        title="Search"
+        onPress={searchGyphy}
+      />
+
+      <ScrollView >
+        {
+          data.length > 5 ? data.map((el, i) => {
+            return (
+              <View
+                key={i}
+                style={{ width: 500, height: 200 }}
+              >
+                <Image
+                  style={{ width: 200, height: 200 }}
+                  source={{ uri: el.images.original.url }}
+                />
+                <Text>
+                  {el.title}
+                </Text>
+              </View>
+            )
+          })
+            : null
+        }
+      </ScrollView>
+
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#ccc',
+    overflow:'hidden'
   },
-});
+  input: {
+    width: '80%',
+    borderWidth: 2,
+    borderColor: 'red',
+    margin: 10,
+    marginTop: 35
+  },
+  btn: {
+    width: '70%',
+    backgroundColor: 'red',
+    color: 'red'
+  }
+})
---
+++ Config/Config.js
@@ -0,0 +1,3 @@
+
+export const API = "http://api.giphy.com/v1/gifs/search?q="
+export const KEY = "&api_key=3TQi2zymLz7a8nLNK385kNn3VT7KklcP&limit=10"
